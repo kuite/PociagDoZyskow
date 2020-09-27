6340437e460edbde531952ef33d7e296f54a66a9
kuite <piomost@gmail.com>
2020-09-28 00:19:18 +0200

    add start project

---
+++ PociagDoZyskow.Start/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PociagDoZyskow.DataAccess.Entities;
+
+namespace PociagDoZyskow.Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var model = new Company();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
